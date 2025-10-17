3df25cbe6f2dc2a04617364e1db8c003747fe4db
Gabriel de Almeida Martins <gabriel_241@outlook.com.br>
2025-10-17 03:07:10 -0300

    Atualizando a Develop com as mudanças da style/reestilizacao-completa

--- login/script.js
+++ login/script.js
@@ -1,24 +1,9 @@
-const form = document.getElementById("loginForm");
-const usuario = document.getElementById("usuario");
-const senha = document.getElementById("senha");
-const mensagem = document.getElementById("mensagem");
+function scaleContainer() {
+  const container = document.querySelector('.container-container');
+  const baseHeight = 620;
+  const scale = window.innerHeight / baseHeight;
+  container.style.transform = `scale(${scale})`;
+}
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    if(usuario.value === "" || senha.value === ""){
-        mensagem.textContent = "Preencha todos os campos!";
-        return;
-    }
-
-    const dadosSalvos = JSON.parse(localStorage.getItem("cadastro"));
-
-    if (dadosSalvos && usuario.value === dadosSalvos.usuario && senha.value === dadosSalvos.senha) {
-        mensagem.style.color = "#03dac6";
-        mensagem.textContent = "Login realizado com sucesso!";
-        localStorage.setItem("usuarioLogado", usuario.value);
-        window.location.href = "../dashboard/dashboard.html";
-    } else {
-        mensagem.textContent = "Usuário ou senha incorreta.";
-    }
-});
+window.addEventListener('resize', scaleContainer);
+scaleContainer();
